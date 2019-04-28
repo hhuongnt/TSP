418185d8ce176e778d846213512eff14537ba5b1
nhuong <n16dcat026@student.ptithcm.edu.vn>
2019-04-28 17:03:45 +0700

    *

--- test_TSP.py
+++ test_TSP.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 import argparse
+from math import pow, sqrt
 
 
 def take_filename():
@@ -13,21 +14,32 @@ def take_filename():
     return filename
 
 
-def readfile(filename):
+def get_node_city(filename):
     """
     Read the input file and return 3 values in the data list
     @parameters:
     data[0]: [city name, latitude, longtitude]
     """
-    data = []
+    node_data = []
     try:
         content = [line.rstrip('\n').lstrip(' ') for line in open(filename)]
         for line in content:
             splited_line = line.split(',')
-            data.append(splited_line)
-        return data
+            node_data.append(splited_line)
+        return node_data
     except FileNotFoundError:
         print ('File not found')
 
 
-print (take_filename())
+class Node(lati, longti, city_name):
+    def _init_():
+        self.lati = lati
+        self.longti = longti
+        self.city_name = city_name
+
+
+def get_distance(start_city, end_city):
+    lati_distance = end_city.lati - start_city.lati
+    longti_distance = end_city.longti - start_city.longti
+    distance = sqrt(pow(lati_distance,2) + pow(longti_distance,2))
+    return distance
