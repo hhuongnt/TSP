3d5467057fa23a16f628e5d11b043a688c0bc063
kaorisatoh <kiara.lion98@gmail.com>
2019-05-08 22:52:52 +0700

    *

--- test_TSP.py
+++ test_TSP.py
@@ -14,42 +14,77 @@ def take_filename():
     return filename
 
 
-def get_node_city(filename):
+
+def get_city_data(filename):
     """
     Read the input file and return 3 values in the data list
     @parameters:
     data[0]: [city name, latitude, longtitude]
     """
-    node_data = []
+    city_data = []
     try:
         content = [line.rstrip('\n').lstrip(' ') for line in open(filename)]
         for line in content:
-            splited_line = line.split(',')
-            node_data.append(splited_line)
-        return node_data
-    except FileNotFoundError:
+            splited_line = line.split(', ')
+            city_data.append(splited_line)
+        return city_data
+    except KeyError:
         print ('File not found')
 
 
+def get_node_cities(dict, city_data):
+    for city in city_data:
+        node = Node(city[0], city[1], city[2])
+        dict.add_node(node)
+    return dict
+
+
 class Node:
-    def __init__(self, latitude, longtitude, city_name):
-        self.lati = float(latitude)
-        self.longti = float(longtitude)
+    """
+    Object have 3 attributes: city_name, latitude, longtitude
+    """
+    def __init__(self, city_name, latitude, longtitude):
         self.city_name = city_name
+        self.latitude = latitude
+        self.longtitude = longtitude
 
 
 class Graph:
-    def __init__(self, distance):
-        self.distance = distance
+    def __init__(self, list_node):
         self.list_node = []
 
 
-    def add_node(node):
+    def add_node(self, node):
         self.list_node.append(node)
 
 
-    def get_distance(start_city, end_city):
-        lati_distance = end_city.lati - start_city.lati
-        longti_distance = end_city.longti - start_city.longti
+    def get_distance(self, node1, node2):
+        """
+        Get distance between two cities
+        @parameters:
+        node1: start_city
+        node2: end_city
+        """
+        lati_distance = abs(float(node2.lati) - float(node1.lati))
+        longti_distance = abs(float(node2.longti) - float(node1.longti))
         distance = sqrt(pow(lati_distance,2) + pow(longti_distance,2))
         return distance
+
+
+    def find_path(self, list_node):
+        path_of_cities = []
+        total_distance = 0
+        start_city = node_cities.pop(0)
+        while node_cities:
+            dictionary_of_cities = {}
+            for city in node_cities:
+                distance = Graph.get_distance(start_city, city)
+                dictionary_of_cities[city] = distance
+        return dictionary_of_cities
+
+filename = take_filename()
+city_data = get_city_data(filename)
+list_node = []
+dict = Graph(list_node)
+get_node_cities(dict, city_data)
+print (dict.list_node)
